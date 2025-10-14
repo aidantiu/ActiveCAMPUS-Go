a1426f00c0a8fa3aa1add24248e94b1261efc1a2
Earl Clyde Bañez <earlclyde.mbanez@gmail.com>
2025-10-15 02:02:01 +0800

    feat: intro 1-3

---
+++ activecampus-go/app/intro1/page.tsx
@@ -0,0 +1,5 @@
+import Intro1 from '../components/Intro1';
+
+export default function Intro1Page() {
+  return <Intro1 />;
+}
---
+++ activecampus-go/app/intro2/page.tsx
@@ -0,0 +1,5 @@
+import Intro2 from '../components/Intro2';
+
+export default function Intro2Page() {
+  return <Intro2 />;
+}
